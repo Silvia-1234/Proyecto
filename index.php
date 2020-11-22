cf0334eb0a9e85e5225fd0f07e6093197746c35b
grover-gitlab <amgroversistemas@gmail.com>
2020-11-21 20:16:32 -0400

    version 1

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php include("include/Encabezado.php") ?>
+
+
+<div >
+
+	
+
+
+
+        <div >
+                
+                    <div >
+                        <h1><center><BR> Reserva de viajes</BR></h1>	
+                    </div>
+        
+                
+            <center>
+                <a href="reservas.php">
+                <button ><font size="4">RESERVAS</font><br><br><img src="images/reserva.png" width="150"></button>
+                </a>
+                        <br>
+                <a href="precios.php">				
+                <button ><font size="4">PRECIOS</font><br><br><img src="images/precio.png" width="150"></button>
+                </a>
+
+                <a href="contacto.php">				
+                <button ><font size="4">CONTACTO</font><br><br><img src="images/contacto.png" width="150"></button>
+                </a>				
+            </center>
+
+        </div>
+      
+
+
+
+
+
+
+
+
+<?php include("include/footer.php") ?>
